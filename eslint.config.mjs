a027f45cc565f5f5d530e589fec66029430354f9
Han Shen Ng <endeavorforconsummation@gmail.com>
2025-09-03 01:24:23 +0800

    fix - eslint rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "no-unused-vars": "warn",
+      "react/react-in-jsx-scope": "off",
+    },
   },
 ];
 
