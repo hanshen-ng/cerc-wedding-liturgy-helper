2c2bfd1b7b12c9a070e1cb37108eea1407d1a0dc
Han Shen Ng <endeavorforconsummation@gmail.com>
2025-09-03 01:11:54 +0800

    feat - initial db migration

---
+++ prisma/migrations/20250902165105_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "public"."Liturgy" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+
+    CONSTRAINT "Liturgy_pkey" PRIMARY KEY ("id")
+);
